9cb5af5fbae5872ed65c2c346ffda418dada362a
Dasitha Samarasinghe <65618412+Dazzy17@users.noreply.github.com>
2022-12-13 21:53:01 +0530

    logging methods

---
+++ src/application_logger/custom_logger.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+
+class CustomApplicationLogger:
+    def __init__(self) -> None:
+        pass
+
+    def logger(self, file_obj, msg):
+        """ 
+        Input: 
+            file_obj: file object 
+            msg: message to be logged 
+        Output: 
+            Loggings the message to the file object 
+        """
+
+        self.now = datetime.now()
+        self.date = self.now.date()
+        self.current_time = self.now.strftime("%H:%M:%S")
+        file_obj.write(
+            str(self.date) + "/" + str(self.current_time) + "\t\t" + msg + "\n"
+        )
